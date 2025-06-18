b6cb32861e4b0b0ff9a343e96f5601c82acb4225
roshansunuwar <rsunuwar999@gmail.com>
2025-06-18 07:25:59 +0545

    initiating the react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  const name = 'Techspire'
+  return (
+    <>
+    
+      <h1>Hello {name}!</h1>
+      <p>Welcome to the world of React.</p>
+      <p>I am react.</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/MyUI.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyUI = () => {
+  return (
+    <div>MyUI</div>
+  )
+}
+
+export default MyUI
